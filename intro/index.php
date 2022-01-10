0b996bcf5b759f09f124eb9ddf9cbfe3007e8150
MiSki <skibamichal@interia.pl>
2022-01-10 23:28:34 +0100

    ready for phase 2

---
+++ intro/index.php
@@ -0,0 +1,15 @@
+	<?php 
+	$title = "Index";
+	include "includes/header.php"
+	?>
+	<h1>Hello world</h1>
+
+	<?php
+	$age = 21;
+	$name = 'Michał';
+	// echo '<h3>My name is ' . $name . ' and my age is ' . $age . ' 😀</h3>';
+	echo "<h3>My name is $name  and my age is  $age 😀</h3>";
+
+	echo "<button class='btn btn-primary'>Click me</button>"; 
+		include "includes/footer.php";
+	?>
